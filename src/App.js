8f1c23622994ee43106191d98be24c2a599c567d
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-06-15 16:32:32 -0400

    added initial files and edits

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// You can just put extends component if you
+class App extends Component {
+  // render is a lifecycle method
+  render() {
+    return (
+      // React.Fragment can be used instead of div and it won't show up in dev tools, "ghost element"
+      <div>
+        <Navbar />
+        <UserItem />
+      </div>
+    );
+  }
 }
 
 export default App;
