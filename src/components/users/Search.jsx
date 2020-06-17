472fffef282f54e8b29d46b3c2ad305c938106b7
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-06-17 15:04:23 -0400

    added clear button

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,19 +1,29 @@
 // rce for  class based component shortcut emit shortcut
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 class Search extends Component {
   state = {
     text: ''
   };
 
+  static propTypes = {
+    //   ptfr emit shortcut
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+  };
+
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
 
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -31,6 +41,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
