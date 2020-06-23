18ef784f5059eb88e3d427b3a8544f0f2b7266eb
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-06-23 15:27:51 -0400

    started building out user profiles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -13,6 +14,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -38,6 +40,17 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Get single github user
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_REACTFINDER_CLIENT_ID}&client_secret=${process.env.REACT_APP_REACTFINDER_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   // Clear Users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -52,7 +65,7 @@ class App extends Component {
 
   // render is a lifecycle method
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       // React.Fragment can be used instead of div and it won't show up in dev tools, "ghost element"
@@ -78,6 +91,18 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" component={About}></Route>
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
