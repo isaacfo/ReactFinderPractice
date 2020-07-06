7c8f146a61051a3b5296bafd14d925d7a734850c
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-07-06 15:29:08 -0400

    Updates

---
+++ precache-manifest.16db7d70a61b82ba7b5ceb7c6acebe77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faf0e3c30931d7223020204690e8ea2",
+    "url": "/isaacfo/ReactFinderPractice/index.html"
+  },
+  {
+    "revision": "6ffadbfdda0842a54e4c",
+    "url": "/isaacfo/ReactFinderPractice/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "cc08eb83c04dd0bfff3d",
+    "url": "/isaacfo/ReactFinderPractice/static/js/2.017736ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/isaacfo/ReactFinderPractice/static/js/2.017736ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffadbfdda0842a54e4c",
+    "url": "/isaacfo/ReactFinderPractice/static/js/main.439aeefa.chunk.js"
+  },
+  {
+    "revision": "f820c8ca62c697010f70",
+    "url": "/isaacfo/ReactFinderPractice/static/js/runtime-main.241994c3.js"
+  }
+]);
