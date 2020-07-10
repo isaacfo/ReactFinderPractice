a6ee7f5d3160ed6c050d6e50ac07aac028a5005d
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-07-10 13:05:03 -0400

    Updates

---
+++ precache-manifest.07b5e5ac4a1bddaba1b717f5a3eab9ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f1bb50c5a42b15a15fce21dfd20302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f59c4f2f0f14f3e7c1ad",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "0be9e97d8edec5ee6652",
+    "url": "/static/js/2.aed2de2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aed2de2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59c4f2f0f14f3e7c1ad",
+    "url": "/static/js/main.2f94ff64.chunk.js"
+  },
+  {
+    "revision": "639511630bdda08457e5",
+    "url": "/static/js/runtime-main.215307ee.js"
+  }
+]);
